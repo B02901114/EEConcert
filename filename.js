14cb5eee45a62464ea16b70c9d20170991ae94b5
Tzu-Ray Su <b01901007@ntu.edu.tw>
2015-05-22 18:05:59 +0800

    Uploaded at Fri, 22 May 2015 10:06:01 GMT

--- filename.js
+++ filename.js
@@ -60,5 +60,6 @@ imgElements = [
 '<img alt="" src="photos/Fri, 22 May 2015 10:04:24 GMT.jpg">',
 '<img alt="" src="photos/Fri, 22 May 2015 10:04:23 GMT.jpg">',
 '<img alt="" src="photos/Fri, 22 May 2015 10:05:23 GMT.jpg">',
-'<img alt="" src="photos/Fri, 22 May 2015 10:05:55 GMT.jpg">'
+'<img alt="" src="photos/Fri, 22 May 2015 10:05:55 GMT.jpg">',
+'<img alt="" src="photos/Fri, 22 May 2015 10:06:01 GMT.jpg">'
 ]
