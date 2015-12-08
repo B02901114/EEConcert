89012be48a821e9bbeff038f0f40188bef3c7841
B02901114 <a83100914@yahoo.com.tw>
2015-12-08 22:29:25 +0800

    Uploaded at Tue, 08 Dec 2015 14:29:19 GMT

--- filename.js
+++ filename.js
@@ -1,4 +1,5 @@
 imgElements = [
 ,
-'<img alt="" src="photos/Tue, 08 Dec 2015 14:24:38 GMT.jpg">'
+'<img alt="" src="photos/Tue, 08 Dec 2015 14:24:38 GMT.jpg">',
+'<img alt="" src="photos/Tue, 08 Dec 2015 14:29:19 GMT.jpg">'
 ]
