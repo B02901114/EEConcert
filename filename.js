f57574a30548acb23e56241a90fb9351ab839f8a
B02901114 <a83100914@yahoo.com.tw>
2015-12-11 17:51:14 +0800

    Uploaded at Fri, 11 Dec 2015 09:51:10 GMT

--- filename.js
+++ filename.js
@@ -18,5 +18,6 @@ imgElements = [
 '<img alt="" src="photos/Fri, 11 Dec 2015 09:46:29 GMT.jpg">',
 '<img alt="" src="photos/Fri, 11 Dec 2015 09:46:27 GMT.jpg">',
 '<img alt="" src="photos/Fri, 11 Dec 2015 09:50:21 GMT.jpg">',
-'<img alt="" src="photos/Fri, 11 Dec 2015 09:50:04 GMT.jpg">'
+'<img alt="" src="photos/Fri, 11 Dec 2015 09:50:04 GMT.jpg">',
+'<img alt="" src="photos/Fri, 11 Dec 2015 09:51:10 GMT.jpg">'
 ]
